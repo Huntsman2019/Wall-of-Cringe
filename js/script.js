72187c94843ee190f64b06926fe4a2fd810a2bbf
Huntsman2019 <vatsallego@gmail.com>
2020-07-06 18:24:24 +0530

    Added a few pending quotes

--- js/script.js
+++ js/script.js
@@ -17,3 +17,20 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function myFunction() {
+  var node = document.createElement("DIV");
+  var node2= document.createElement("P")
+  var textnode = document.createTextNode(document.getElementById("quotes").value);
+  node2.appendChild(textnode);
+  node.appendChild(node2);
+  document.getElementById("main-content").appendChild(node);
+  node2.className="quote";
+  node2= document.createElement("P")
+  textnode = document.createTextNode("-"+document.getElementById("names").value);
+  node2.appendChild(textnode);
+  node.appendChild(node2);
+  document.getElementById("main-content").appendChild(node);
+  node2.className="quoted-by";
+  node.classList="col-12 col-sm-4";
+}
